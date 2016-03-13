0d89953df0a6ab0267bfa37e5b6398a4e18dab21
Dankller <danielhungkim@gmail.com>
2016-03-12 17:23:49 -0800

    [setup] created mongodb connection

--- server/server.js
+++ server/server.js
@@ -6,12 +6,19 @@ var path = require('path');
 var app = express();
 var server = http.createServer(app).listen(process.env.PORT || 3000);
 var io = require('socket.io')(server);
+var mongoose = require('mongoose');
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/../client/'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use('/node_modules', express.static(__dirname + '/../node_modules'));
+
+// var mongooseUsername = process.env.USERNAME || config.username;
+// var mongoosePassword = process.env.PASSWORD || config.password;
+
+mongoose.connect('mongodb://' + 'townhall' + ':' + 'makersquare32' + '@ds011369.mlab.com:11369/townhall');
+console.log('Connected to Mongoose');
 
 io.on('connection', function(socket) {
   socket.on('change', function(card) {
