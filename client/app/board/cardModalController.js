35ee804cf0a25134c363f38b16bdf30b8255dcd5
Richard Yu <ryu323@gmail.com>
2016-03-19 15:29:17 -0700

    [feature] added ability to add comments, not saving to database yet

--- client/app/board/cardModalController.js
+++ client/app/board/cardModalController.js
@@ -1,7 +1,26 @@
-TownHall.controller('cardModalCtrl', function($scope, $mdDialog, card) {
-
+TownHall.controller('cardModalCtrl', function($scope, $window, $state, $mdDialog, card) {
 
   $scope.card = card;
+  $scope.comment = "";
+
+  $scope.addComment = function(comment) {
+    var user = localStorage.getItem('firebase:session::townhallapp');
+    if(user) {
+      console.log('current user', user);
+      card.comments.push({attachments: "", createdBy: "user information goes here", text: comment});
+      $scope.comment = "";
+    } else {
+      var confirm = $mdDialog.confirm()
+        .title('Please Sign In')
+        .textContent('Something went wrong, you must be signed in to comment')
+        .ariaLabel('Sign In')
+        .ok('Sign In')
+        .cancel('Cancel');
+      $mdDialog.show(confirm).then(function() {
+        $state.go('signin');
+      });
+    }
+  }
 
 
 });
