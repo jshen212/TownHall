d0bde98d8a5335eda60fae8921c28fcd370d74b9
Dankller <danielhungkim@gmail.com>
2016-03-24 11:16:11 -0700

    [bug] fixed empty function

--- client/app/chat/chatFactory.js
+++ client/app/chat/chatFactory.js
@@ -1,16 +1,13 @@
 TownHall.factory('Chat', function($http, $firebaseArray, FirebaseUrl, $window, $state, $firebaseAuth) {
 
 
-  var createChat = function(boardId){
+  var createChat = function(boardId) {
     var boardMessagesRef = new Firebase('https://townhallapp.firebaseio.com/' + boardId);
     // boardMessagesRef.push({name: "yo"});
   };
 
-
-
   return {
     createChat: createChat
-    // sendChat: sendChat
   };
 
 
