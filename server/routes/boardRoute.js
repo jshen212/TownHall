6e6b994c20a63daad74e11f4410fee900a78da13
Dankller <danielhungkim@gmail.com>
2016-03-28 15:17:41 -0700

    [bug] aws fix

--- server/routes/boardRoute.js
+++ server/routes/boardRoute.js
@@ -1,22 +1,13 @@
 var boardController = require('../controllers/boardSQLController.js');
 
 module.exports = function(app) {
-  // make board
   app.post('/create', boardController.createBoard);
-  //
   app.post('/board', boardController.getBoard);
-  // get
   app.post('/profileView', boardController.getProfileView);
-  // update board
-  app.post('/update', boardController.updateBoard);
-  // delete board
-  app.post('/delete', boardController.deleteBoard);
-  // send invites(create invitations)
-  app.post('/invite', boardController.sendInvite);
-  // get invititations
-  app.post('/invitations', boardController.getInviteBoards);
-  // update invitations
   app.post('/populateRoom', boardController.getBoardMembers);
-  // hi
+  app.post('/update', boardController.updateBoard);
+  app.post('/delete', boardController.deleteBoard);
+  app.post('/invite', boardController.sendInvite);
+  app.post('/invitations', boardController.getInviteBoards);
   app.post('/updateInvite', boardController.updateInvite);
 };
