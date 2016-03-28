cf46071142705fa19c10615dfdba5c8e3c76d543
Dankller <danielhungkim@gmail.com>
2016-03-28 15:30:35 -0700

    [bug] undid routing deletion

--- server/routes/boardRoute.js
+++ server/routes/boardRoute.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
   app.post('/profileView', boardController.getProfileView);
   app.post('/populateRoom', boardController.getBoardMembers);
   app.post('/update', boardController.updateBoard);
-  // app.post('/delete', boardController.deleteBoard);
+  app.post('/delete', boardController.deleteBoard);
   app.post('/invite', boardController.sendInvite);
   app.post('/invitations', boardController.getInviteBoards);
   app.post('/updateInvite', boardController.updateInvite);
